e77f001de0e2c8b6ccf8ae2550538f986130b6a8
Joel <joelvieirasouza@gmail.com>
2023-11-23 21:30:29 -0300

    Create add-meetings.page.js

---
+++ scripts/pages/meetings/add-meetings.page.js
@@ -0,0 +1,95 @@
+import { postMeetings } from '../../services/meeting.service.js'
+import { buscaLider } from '../../services/lideres.service.js'
+import { getCollaborators } from '../../services/collaborators.service.js'
+
+
+const addMeetings = document.createElement('form')
+addMeetings.setAttribute('id', 'p-add-meetings')
+
+const eventos = () => {
+    addMeetings.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        const fd = new FormData(addMeetings)
+        
+        const selectLider = document.getElementById('id_lider')
+        const selectedValue = selectLider.options[selectLider.selectedIndex].value
+
+        const selectColaborador = document.getElementById('id_colaborador')
+        const selectedValue2 = selectColaborador.options[selectColaborador.selectedIndex].value
+
+        fd.append('id_lider', selectedValue)
+        fd.append('id_colaborador', selectedValue2)
+
+        console.log(Object.fromEntries(fd))
+
+        postMeetings(Object.fromEntries(fd))
+            .then(() => {
+                window.alert("Reunião criada com sucesso")
+                window.location.reload()
+            })
+            .catch((erro) => {
+                console.log(erro)
+            })
+    })
+
+    buscaLider()
+    .then((data) => {
+        if (data) {
+            const selectLider = document.getElementById('id_lider')
+            data.forEach(element => {
+                const option = document.createElement('option')
+                option.setAttribute('value', element.id)
+                option.textContent = element.nome
+                selectLider.appendChild(option)
+            });
+        } else {
+            console.log('Sem dados', data)
+        }
+    })
+
+    getCollaborators()
+    .then((data) => {
+        if (data) {
+            const selectColaborador = document.getElementById('id_colaborador')
+            data.forEach(element => {
+                const option = document.createElement('option')
+                option.setAttribute('value', element.id)
+                option.textContent = element.nome
+                selectColaborador.appendChild(option)
+            });
+        } else {
+            console.log('Sem dados', data)
+        }
+    })
+
+}
+
+export const AddMeetings = () => {    
+    addMeetings.innerHTML = `
+    <div class="container">
+        <div class="card">    
+            <form action="">
+            <h1 class="title">Criar ou Editar Reunião</h1>
+            
+                <label for="lider" class="form-label">Líder</label>
+                <select class="lider" id="id_lider"></select>
+            
+            
+                <label for="colaborador">Colaborador</label>
+                <select class="lider" id="id_colaborador"></select>
+
+                <label for="data">Data</label>
+                <input class="data" type="date" id="data" name="data_reuniao" placeholder="Digite a data">
+            
+            </form>
+            <div class="buttons">
+            <button class="btn btn-primary">Agendar</button>
+            </div>
+        </div>  
+    </div>
+
+    `
+    eventos()
+    return addMeetings
+}
