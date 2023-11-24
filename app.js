6c6846dce52c13a4a9d58d698a5023546edf522e
Joel <joelvieirasouza@gmail.com>
2023-11-23 21:29:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import { NotFound } from './scripts/pages/404.page.js'
 import { Header } from './scripts/pages/header.page.js'
-import { Leaders } from './scripts/pages/leaders.page.js'
-import { AddLeaders } from './scripts/pages/add-leaders.page.js'
-import { Collaborators } from './scripts/pages/collaborators.page.js'
-import { Reunioes } from './scripts/pages/reunioes.page.js'
+import { Leaders } from './scripts/pages/leader/leaders.page.js'
+import { AddLeaders } from './scripts/pages/leader/add-leaders.page.js'
+import { Collaborators } from './scripts/pages/collaborator/collaborators.page.js'
+import { AddCollaborator } from './scripts/pages/collaborator/add-collaborator.page.js'
+import { Meetings } from './scripts/pages/meetings/meetings.page.js'
+import { AddMeetings } from './scripts/pages/meetings/add-meetings.page.js'
 import { Signup } from './scripts/pages/signup.page.js'
 import { Login } from './scripts/pages/login.page.js'
 
@@ -13,7 +15,9 @@ const ROUTER = {
     "#leaders": { component: Leaders, private: false, nome: 'leaders' },
     "#add-leaders": { component: AddLeaders, private: false, nome: 'add-leaders' },
     "#collaborators": { component: Collaborators, private: false, nome: 'collaborators' },
-    "#reunioes": { component: Reunioes, private: false, nome: 'reunioes' },
+    "#add-collaborator": { component: AddCollaborator, private: false, nome: 'add-collaborator' },
+    "#meetings": { component: Meetings, private: false, nome: 'meetings' },
+    "#add-meetings": { component: AddMeetings, private: false, nome: 'add-meetings' },
     "#signup": { component: Signup, private: false, nome: 'signup' },
     "#login": { component: Login, private: false, nome: 'login' }
 }
