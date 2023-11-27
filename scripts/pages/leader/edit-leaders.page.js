927226746d1e531eeedf4d59506c92ff5ea9a580
Joel <joelvieirasouza@gmail.com>
2023-11-26 22:28:45 -0300

    Update edit-leaders.page.js

--- scripts/pages/leader/edit-leaders.page.js
+++ scripts/pages/leader/edit-leaders.page.js
@@ -1,4 +1,4 @@
-import { buscaLider } from "../../services/lideres.service.js";
+import { getAllLeaders, editarLider } from "../../services/lideres.service.js";
 
 const editLeader = document.createElement('div')
 editLeader.setAttribute('id', 'p-editar-lider')
@@ -8,62 +8,58 @@ const eventos = () => {
     const params = url.searchParams
     const idLider = params.get('id')
 
-    buscaLider(idLider)
-        .then(( data ) => {
-            console.log(data)
+    getAllLeaders(idLider)
+        .then((data) => {
+            console.log(data);
 
             editLeader.innerHTML = `
                 <div class="container">
                     <div class="card-leader">
-                        <h1 class="title">Editar Líder  ${data[idLider].nome}</h1>
+                        <form id="editForm">
+                            <h1 class="title">Editar Líder</h1>
 
-                        <label for="nome" class="form-label">Nome</label>
-                        <input class="nome" type="text" id="nome" name="nome" placeholder="Digite o nome">
+                            <label for="nome" class="form-label">Nome</label>
+                            <input class="nome" type="text" id="nome" name="nome" value="${data.nome || ''}">
 
-                        <label for="departamento">Departamento</label>
-                        <input class="departamento" type="text" id="departamento" name="departamento" placeholder="Digite o departamento">
+                            <label for="departamento">Departamento</label>
+                            <input class="departamento" type="text" id="departamento" name="departamento" value="${data.departamento || ''}">
 
-                        <label for="email">E-mail</label>
-                        <input class="email" type="email" id="email" name="email" placeholder="Digite o e-mail">
+                            <label for="email">E-mail</label>
+                            <input class="email" type="email" id="email" name="email" value="${data.email || ''}">
 
-                        <div class="buttons">
-                            <button class="btn btn-primary">Cadastrar</button>
-                        </div>
+                            <div class="buttons">
+                                <button type="button" class="btn btn-primary" id="editarBtn">Editar</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
+            `;
 
-                    `
+            const editForm = editLeader.querySelector('#editForm');
+            const editarBtn = editLeader.querySelector('#editarBtn');
+
+            editarBtn.addEventListener('click', () => {
+                const formData = new FormData(editForm);
+                const dadosFormulario = Object.fromEntries(formData);
+
+                editarLider(idLider, dadosFormulario)
+                    .then(() => {
+                        window.alert("Líder editado com sucesso");
+                        window.location.reload();
+                    })
+                    .catch((erro) => {
+                        console.log(dadosFormulario);
+                        console.log(erro);
+                    });
+            });
         })
         .catch((erro) => {
-            console.log(erro)
-        })
+            console.log(erro);
+        });
 }
 
 export const EditLeaders = () => {
-    editLeader.innerHTML = `
-    <div class="container">
-    <div class="card-leader">    
-        <form action="">
-        <h1 class="title">Editar Líder</h1>
-          
-            <label for="nome" class="form-label">Nome</label>
-            <input class="nome" type="text" id="nome" name="nome" placeholder="Digite o nome">
-          
-          
-            <label for="departamento">Departamento</label>
-            <input class="departamento" type="text" id="departamento" name="departamento" placeholder="Digite o departamento">
-
-            <label for="email">E-mail</label>
-            <input class="email" type="email" id="email" name="email" placeholder="Digite o e-mail">
-          
-        </form>
-        <div class="buttons">
-          <button class="btn btn-primary">Cadastrar</button>
-        </div>
-    </div>  
-  </div>
-`
-    eventos()
+    eventos();
     return editLeader;
 }
 
